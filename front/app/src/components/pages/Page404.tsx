6b5f16bbb8358169c3c6873a3e6d5cbab44bb346
yt <asnedge.yt@gmail.com>
2022-01-25 01:10:03 +0900

    initial commit

---
+++ front/app/src/components/pages/Page404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Page404 = () => {
+    return (
+        <div>
+            <h1>This Page is 404</h1>
+            <h2>This Page is 404</h2>
+            <h3>This Page is 404</h3>
+            <h4>This Page is 404</h4>
+            <h5>This Page is 404</h5>
+        </div>
+    );
+};
